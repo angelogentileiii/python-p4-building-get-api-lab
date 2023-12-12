f1350d845102702b1c55c6bdf7f1add67e85c9dd
Angelo Gentile III <angelo.john.gentile@gmail.com>
2023-12-12 13:52:56 -0500

    completed lab

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
+from sqlalchemy import desc, asc
 
 from models import db, Bakery, BakedGood
 
@@ -20,19 +21,43 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()
+    if not bakeries:
+        return {'Error': 'There are currenty no bakeries in database.'}, 404
+
+    bakeries_data = [b.to_dict() for b in bakeries]
+
+    return bakeries_data, 200
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.get(id)
+
+    if not bakery:
+        return {'Error': f'Bakery with id {id} does not currently exist.'}, 404
+
+    return bakery.to_dict(), 200
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    sorted_baked_goods = BakedGood.query.order_by(desc(BakedGood.price)).all()
+
+    if not sorted_baked_goods:
+        return make_response(jsonify({'Error': 'There are no baked goods in the database.'}), 404)
+
+    sbg_data = [s.to_dict() for s in sorted_baked_goods]
+
+    return make_response(jsonify(sbg_data), 200)
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    sorted_baked_goods = BakedGood.query.order_by(desc(BakedGood.price)).first()
+
+    if not sorted_baked_goods:
+        return make_response(jsonify({'Error': 'There are no baked goods in the database.'}), 404)
+
+    return make_response(jsonify(sorted_baked_goods.to_dict()), 200)
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
